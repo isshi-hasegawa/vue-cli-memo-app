55bd547efce4ed74f93562c6979d56f0039569bf
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-04-14 20:34:49 +0900

    メモの追加と一覧を実装

--- src/components/VueCliMemo.vue
+++ src/components/VueCliMemo.vue
@@ -1,9 +1,51 @@
 <template>
+  <div>
+    <ul>
+      <li v-for="memo in memos" :key="memo">
+        <span>{{ memo.split('\n')[0] }}</span>
+      </li>
+    </ul>
 
+    <div @click="displayForm">＋</div>
+    <div v-if="isAddable">
+      <textarea v-model="newMemo"></textarea>
+      <div>
+        <button @click="addMemo">Add</button>
+        <button @click="cancel">Cancel</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      newMemo: '',
+      memos: [],
+      isAddable: false,
+    }
+  },
+  mounted () {
+    this.memos = JSON.parse(localStorage.getItem('memos')) || [];
+  },
+  methods: {
+    displayForm(){
+      this.isAddable = true
+      this.newMemo = ''
+    },
+    addMemo () {
+      this.memos.push(this.newMemo)
+      this.saveMemo()
+    },
+    saveMemo () {
+      localStorage.setItem('memos', JSON.stringify(this.memos))
+      this.cancel()
+    },
+    cancel () {
+      this.isAddable = false
+      this.newMemo = ''
+    }
+  },
 }
 </script>
